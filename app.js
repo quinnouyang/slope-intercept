c0ee01c6906df351b89f32f618e925ca57ed7531
quinnouyang <quinn.ouy@gmail.com>
2022-04-12 22:52:27 -0500

    Completed MVP

--- app.js
+++ app.js
@@ -1,78 +1,125 @@
-const GRAPH_DIMENSION = 800;
+// DEFAULT VALUES/SETTINGS: EDITABLE
+const QUADRANT_BOXES = 10;
+const DEFAULT_SLOPE = 1;
+const DEFAULT_Y_INTERCEPT = 3;
+const LABEL_SCALING = 8; // Arbitrary scaling offset
+
+const GRAPH_DIMENSION = 800; // Buggy if edited because of CSS flex conflicts
 const QUADRANT_DIMENSION = GRAPH_DIMENSION / 2;
-const QUADRANT_BOXES = 15;
 const GRID_BOX_DIMENSION = QUADRANT_DIMENSION / QUADRANT_BOXES;
-let slope = 1;
-let yIntercept = 3 * GRID_BOX_DIMENSION;
+const AXIS_LABELS_FONT_SIZE = QUADRANT_DIMENSION / (2 * QUADRANT_BOXES); // Lazy font-sizing WRT to number of boxes in a quadrant
+
 let emptyGraphCanvas = null;
 
 function drawDefault() {
+  let graph = document.getElementById("graph");
+  graph.width = graph.height = GRAPH_DIMENSION;
+  document.getElementById("slope").value = DEFAULT_SLOPE;
+  document.getElementById("yIntercept").value = DEFAULT_Y_INTERCEPT;
+
   drawCoordinatePlane();
-  drawLine();
+  drawLine(DEFAULT_SLOPE, DEFAULT_Y_INTERCEPT);
+  renderEquation(DEFAULT_SLOPE, DEFAULT_Y_INTERCEPT);
 }
 
 function drawCoordinatePlane() {
-  var graph = document.getElementById("graph");
-  if (!graph.getContext) return;
+  let graph = document.getElementById("graph");
   /** @type {CanvasRenderingContext2D} */
-  var ctx = graph.getContext("2d");
+  let ctx = graph.getContext("2d");
 
+  // Y-axis
   ctx.lineWidth = 4;
   ctx.beginPath();
   ctx.moveTo(QUADRANT_DIMENSION, 0);
   ctx.lineTo(QUADRANT_DIMENSION, GRAPH_DIMENSION);
   ctx.stroke();
 
+  // X-axis
   ctx.beginPath();
   ctx.moveTo(0, QUADRANT_DIMENSION);
   ctx.lineTo(GRAPH_DIMENSION, QUADRANT_DIMENSION);
   ctx.stroke();
 
+  // Axis labels
+  ctx.font = `${AXIS_LABELS_FONT_SIZE}px Arial`;
+  ctx.textAlign = "right";
+  ctx.textBaseline = "top";
   ctx.lineWidth = 1;
-  for (let line = 0; line < GRAPH_DIMENSION; line += GRID_BOX_DIMENSION) {
+
+  for (
+    let line = 0, count = 0;
+    line < GRAPH_DIMENSION;
+    line += GRID_BOX_DIMENSION, count++
+  ) {
+    // Vertical line
     ctx.beginPath();
     ctx.moveTo(0, line);
     ctx.lineTo(GRAPH_DIMENSION, line);
     ctx.stroke();
 
+    // Y-axis label
+    ctx.fillText(
+      Math.round(QUADRANT_DIMENSION / GRID_BOX_DIMENSION - count),
+      QUADRANT_DIMENSION - GRID_BOX_DIMENSION / LABEL_SCALING,
+      line + GRID_BOX_DIMENSION / LABEL_SCALING
+    );
+
+    // Horizontal line
     ctx.beginPath();
     ctx.moveTo(line, 0);
     ctx.lineTo(line, GRAPH_DIMENSION);
     ctx.stroke();
+
+    // X-axis label
+    ctx.fillText(
+      Math.round(count - QUADRANT_DIMENSION / GRID_BOX_DIMENSION),
+      line - GRID_BOX_DIMENSION / LABEL_SCALING,
+      QUADRANT_DIMENSION + GRID_BOX_DIMENSION / LABEL_SCALING
+    );
   }
 
   emptyGraphCanvas = ctx.getImageData(0, 0, GRAPH_DIMENSION, GRAPH_DIMENSION);
 }
 
-function drawLine() {
-  var graph = document.getElementById("graph");
-  if (!graph.getContext) return;
-
+function drawLine(slope, yIntercept) {
+  let graph = document.getElementById("graph");
   /** @type {CanvasRenderingContext2D} */
-  var ctx = graph.getContext("2d");
-  ctx.putImageData(emptyGraphCanvas, 0, 0);
+  let ctx = graph.getContext("2d");
 
   ctx.strokeStyle = "red";
-  console.log("apple");
   ctx.lineWidth = 4;
   ctx.beginPath();
   ctx.moveTo(
     0,
-    QUADRANT_DIMENSION - (slope * -QUADRANT_DIMENSION + yIntercept)
+    QUADRANT_DIMENSION -
+      (slope * -QUADRANT_DIMENSION + yIntercept * GRID_BOX_DIMENSION)
   );
   ctx.lineTo(
     GRAPH_DIMENSION,
-    QUADRANT_DIMENSION - (slope * QUADRANT_DIMENSION + yIntercept)
+    QUADRANT_DIMENSION -
+      (slope * QUADRANT_DIMENSION + yIntercept * GRID_BOX_DIMENSION)
   );
+
+  ctx.putImageData(emptyGraphCanvas, 0, 0); // "Clear" graph
   ctx.stroke();
 }
 
-function handleSlopeInput() {
-  slope = document.getElementById("slope").value;
-  drawLine();
+function renderEquation(slope, yIntercept) {
+  let equation = "y = ";
+  if (slope === 0 && yIntercept === 0) equation += "0";
+  else if (slope !== 0 && slope !== 1) equation += `${slope} x`;
+  else if (slope === 1) equation += "x";
+
+  if (slope !== 0 && yIntercept > 0) equation += "+";
+  if (yIntercept !== 0) equation += `${yIntercept}`;
+
+  katex.render(equation, document.getElementById("equation"));
 }
 
-function handleYInterceptInput() {
-  yIntercept = document.getElementById("yIntercept").value * GRID_BOX_DIMENSION;
-  drawLine();
+function handleInput() {
+  const slope = Number(document.getElementById("slope").value);
+  const scaledYIntercept =
+    document.getElementById("yIntercept").value * GRID_BOX_DIMENSION;
+  drawLine(slope, scaledYIntercept / GRID_BOX_DIMENSION);
+  renderEquation(slope, scaledYIntercept / GRID_BOX_DIMENSION);
 }
